ba30df2e10b00982f0a1de2562fe786068f7100d
LenWinkler <lenwinkler86@gmail.com>
2020-02-12 12:40:33 -0500

    tweaked cal answer

--- src/14_cal.py
+++ src/14_cal.py
@@ -46,7 +46,7 @@ elif len(args) == 2 and int(args[1]) <= 12:
 elif len(args) == 3 and int(args[1]) <= 12:
   print(cal.formatmonth(int(args[2]), int(args[1])))
 
-elif len(args) > 3 or len(args) == 0 or int(args[1]) > 12:
+elif len(args) > 3 or int(args[1]) > 12:
   print('\nError. This program expects arguments in the following format: ** 14_cal.py [year] [month] **. If only one argument is provided, it will assume that the month has been specified and will print that month\'s calender for the current year. If no arguments are passed, it will print the current month.\n')
 
 
--- src/15_classes.py
+++ src/15_classes.py
@@ -3,17 +3,35 @@
 
 # YOUR CODE HERE
 
+class LatLon:
+    def __init__(self):
+        self.lat = lat
+        self.lon = lat
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+
+class Waypoint(LatLon):
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
 
+class Geocache(LatLon, Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
+
+print(Waypoint('test name', 130, -123))
 
 # YOUR CODE HERE
 
@@ -27,3 +45,6 @@ print(waypoint)
 
 # Print it--also make this print more nicely
 print(geocache)
+
+
+# 
